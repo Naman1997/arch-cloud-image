01ce001b1a305578e116c46c259848a77774b88f
Naman Arora <aroranaman17@outlook.com>
2023-08-15 23:45:25 +0530

    Add check in create.sh and fix drive type

--- scripts/create.sh
+++ scripts/create.sh
@@ -4,4 +4,8 @@ sed -i -E "s/(user: ).*/\1$1/" cloud-init/user-data
 sed -i -E "s/(\s+- ).*:/\1$1:/" cloud-init/user-data
 rm -rf output-archlinux
 packer build -var 'username='"$1"'' archlinux.pkr.hcl
+if [ $? != "0" ]; then
+    echo "Unable to create the golden arch image."
+    exit 1
+fi
 echo "Successfully created golden arch image."
